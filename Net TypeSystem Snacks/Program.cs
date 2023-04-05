a0f38b95ae69cf3634ca91472e3fa1135df5b021
Hadson Roque <nineonezero.light@gmail.com>
2023-04-05 17:49:24 +0200

    End

--- Net TypeSystem Snacks/Program.cs
+++ Net TypeSystem Snacks/Program.cs
@@ -1,185 +1,188 @@
-﻿////Snack 1 Istruzioni condizionali
-//int num1, num2;
-//bool isNum1Valid, isNum2Valid;
+﻿//Snack 1 Istruzioni condizionali
+int num1, num2;
+bool isNum1Valid, isNum2Valid;
 
-//do
-//{
-//    Console.Write("Inserisci il primo numero: ");
-//    isNum1Valid = int.TryParse(Console.ReadLine(), out num1);
-//    if (!isNum1Valid)
-//    {
-//        Console.WriteLine("Il valore inserito non è un numero intero valido. Riprova.");
-//    }
-//} while (!isNum1Valid);
+do
+{
+    Console.Write("Inserisci il primo numero: ");
+    isNum1Valid = int.TryParse(Console.ReadLine(), out num1);
+    if (!isNum1Valid)
+    {
+        Console.WriteLine("Il valore inserito non è un numero intero valido. Riprova.");
+    }
+} while (!isNum1Valid);
 
-//do
-//{
-//    Console.Write("Inserisci il secondo numero: ");
-//    isNum2Valid = int.TryParse(Console.ReadLine(), out num2);
-//    if (!isNum2Valid)
-//    {
-//        Console.WriteLine("Il valore inserito non è un numero intero valido. Riprova.");
-//    }
-//} while (!isNum2Valid);
+do
+{
+    Console.Write("Inserisci il secondo numero: ");
+    isNum2Valid = int.TryParse(Console.ReadLine(), out num2);
+    if (!isNum2Valid)
+    {
+        Console.WriteLine("Il valore inserito non è un numero intero valido. Riprova.");
+    }
+} while (!isNum2Valid);
 
-//if (num1 < num2)
-//{
-//    Console.WriteLine($"Il numero più piccolo è: {num1}");
-//}
-//else
-//{
-//    Console.WriteLine($"Il numero più piccolo è: {num2}");
-//}
-
-
-////Snack 2 Istruzioni condizionali
-
-//Console.Write("Inserisci la prima parola: ");
-
-//string parola1 = Console.ReadLine();
-
-//Console.Write("Inserisci la prima parola: ");
-//string parola2 = Console.ReadLine();
-
-//if (parola1.Length < parola2.Length)
-//{
-//    Console.WriteLine($"La parola piu' corta e': {parola1}");
-//}
-
-//else if
-//{
-//    Console.WriteLine($"La parola piu' corta e': {parola2}");
-//}
-//else
-//{
-//    Console.WriteLine("Le parole sono lunghe uguali");
-//}
+if (num1 < num2)
+{
+    Console.WriteLine($"Il numero più piccolo è: {num1}");
+}
+else
+{
+    Console.WriteLine($"Il numero più piccolo è: {num2}");
+}
 
 
-////Snack 3 Ciclo for
-//int somma = 0;
-//for (int i = 0; i < 10; i++)
-//{
-//    Console.Write($"Inserisci il numero: ");
-//    int numero = int.Parse(Console.ReadLine());
+//Snack 2 Istruzioni condizionali
 
-//    somma += numero;
-//}
-//Console.WriteLine("La somma di tutti i numeri inseriti è: " + somma);
+Console.Write("Inserisci la prima parola: ");
 
+string parola1 = Console.ReadLine();
 
-////Snack 4 Ciclo for
-//int somma = 0;
-//for (int i = 2; i < 11; i++)
-//{
-//    Console.WriteLine(i);
-//    somma += i;
+Console.Write("Inserisci la prima parola: ");
+string parola2 = Console.ReadLine();
 
-//}
-//Console.WriteLine("La somma di tutti i numeri inseriti è: " + somma);
+if (parola1.Length < parola2.Length)
+{
+    Console.WriteLine($"La parola piu' corta e': {parola1}");
+    Console.WriteLine($"La parola piu' lunga e': {parola2}");
+}
 
-//int mediana = somma / 9;
-//Console.WriteLine("La mediana di tutti i numeri inseriti è: " + mediana);
-
-
-////Snack 5 Operatore Modulo
-//int num;
-//bool isNumValid;
-
-//do
-//{
-//    Console.Write("Inserisci un numero: ");
-//    isNumValid = int.TryParse(Console.ReadLine(), out num);
-
-//    if (!isNumValid)
-//    {
-//        Console.WriteLine("Il valore inserito non è un numero intero valido. Riprova.");
-//    }
-//} while (!isNumValid);
-
-//if (num % 2 == 0)
-//{
-//    Console.WriteLine("Il valore inserito è pari");
-//}
-//else
-//{
-//    Console.WriteLine("Il valore inserito è dispari");
-//}
+else if
+{
+    Console.WriteLine($"La parola piu' corta e': {parola2}");
+    Console.WriteLine($"La parola piu' lunga e': {parola1}");
+}
+else
+{
+    Console.WriteLine("Le parole sono lunghe uguali");
+}
 
 
-////Snack 6 Array
-//string[] invitati = { "Davide", "Andrea", "Igor", "Romina", "Vale", "Marco" };
+//Snack 3 Ciclo for
+int somma = 0;
+for (int i = 0; i < 10; i++)
+{
+    Console.Write($"Inserisci il numero: ");
+    int numero = int.Parse(Console.ReadLine());
 
-//Console.Write("Inserisci il tuo nome: ");
-//string persona = Console.ReadLine();
-
-//if (invitati.Contains(persona))
-//{
-//    Console.WriteLine("Puoi entrare alla festa");
-//}
-//else
-//{
-//    Console.WriteLine("Non puoi entrare alla festa");
-//}
+    somma += numero;
+}
+Console.WriteLine("La somma di tutti i numeri inseriti è: " + somma);
 
 
-////Snack 7 Array
-//int[] numeriDispari = new int[0];
+//Snack 4 Ciclo for
+int somma = 0;
+for (int i = 2; i < 11; i++)
+{
+    Console.WriteLine(i);
+    somma += i;
 
-//for (int i = 0; i < 6; i++)
-//{
-//    Console.Write("Inserisci i numeri: ");
-//    int numero = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("La somma di tutti i numeri inseriti è: " + somma);
 
-//    if (numero % 2 != 0)
-//    {
-//        Array.Resize(ref numeriDispari, numeriDispari.Length + 1);
-//        numeriDispari[numeriDispari.Length - 1] = numero;
-//    }
-//}
-
-//Console.WriteLine("Numeri dispari inseriti:");
-//foreach (int numero in numeriDispari)
-//{
-//    Console.Write(numero + " ");
-//}
-
-////Snack 8 Array
-//int[] numeriInteri = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-//int somma = 0;
-//for (int i = 0; i < numeriInteri.Length; i++) {
-//    if (i % 2 != 0)
-//    {
-//        somma += numeriInteri[i];
-//    }
-//}               
-//Console.WriteLine(somma);
+int mediana = somma / 9;
+Console.WriteLine("La mediana di tutti i numeri inseriti è: " + mediana);
 
 
-////Snack 9 Ciclo While
-//int[] numeri = new int[0];
-//int somma = 0;
+//Snack 5 Operatore Modulo
+int num;
+bool isNumValid;
 
-//while (somma < 50)
-//{
-//    Console.WriteLine("Inserisci un numero:");
-//    int numero = int.Parse(Console.ReadLine());
-//    somma += numero;
+do
+{
+    Console.Write("Inserisci un numero: ");
+    isNumValid = int.TryParse(Console.ReadLine(), out num);
 
-//    Array.Resize(ref numeri, numeri.Length + 1);
-//    numeri[numeri.Length - 1] = numero;
-//}
+    if (!isNumValid)
+    {
+        Console.WriteLine("Il valore inserito non è un numero intero valido. Riprova.");
+    }
+} while (!isNumValid);
 
-//Console.WriteLine("Somma degli elementi:");
-//Console.WriteLine(somma);
-//Console.WriteLine("Numeri inseriti nell'array:");
-//foreach (int numero in numeri)
-//{
-//    Console.Write(numero + " ");
-//}
+if (num % 2 == 0)
+{
+    Console.WriteLine("Il valore inserito è pari");
+}
+else
+{
+    Console.WriteLine("Il valore inserito è dispari");
+}
 
 
-//Snack 10 Random
+//Snack 6 Array
+string[] invitati = { "Davide", "Andrea", "Igor", "Romina", "Vale", "Marco" };
+
+Console.Write("Inserisci il tuo nome: ");
+string persona = Console.ReadLine();
+
+if (invitati.Contains(persona))
+{
+    Console.WriteLine("Puoi entrare alla festa");
+}
+else
+{
+    Console.WriteLine("Non puoi entrare alla festa");
+}
+
+
+//Snack 7 Array
+int[] numeriDispari = new int[0];
+
+for (int i = 0; i < 6; i++)
+{
+    Console.Write("Inserisci i numeri: ");
+    int numero = int.Parse(Console.ReadLine());
+
+    if (numero % 2 != 0)
+    {
+        Array.Resize(ref numeriDispari, numeriDispari.Length + 1);
+        numeriDispari[numeriDispari.Length - 1] = numero;
+    }
+}
+
+Console.WriteLine("Numeri dispari inseriti:");
+foreach (int numero in numeriDispari)
+{
+    Console.Write(numero + " ");
+}
+
+//Snack 8 Array
+int[] numeriInteri = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+int somma = 0;
+for (int i = 0; i < numeriInteri.Length; i++)
+{
+    if (i % 2 != 0)
+    {
+        somma += numeriInteri[i];
+    }
+}
+Console.WriteLine(somma);
+
+
+/******************************* Snack 9 Ciclo While *********************************************/
+int[] numeri = new int[0];
+int somma = 0;
+
+while (somma < 50)
+{
+    Console.WriteLine("Inserisci un numero:");
+    int numero = int.Parse(Console.ReadLine());
+    somma += numero;
+
+    Array.Resize(ref numeri, numeri.Length + 1);
+    numeri[numeri.Length - 1] = numero;
+}
+
+Console.WriteLine("Somma degli elementi:");
+Console.WriteLine(somma);
+Console.WriteLine("Numeri inseriti nell'array:");
+foreach (int numero in numeri)
+{
+    Console.Write(numero + " ");
+}
+
+
+/******************************* Snack 10 Random *********************************************/
 Console.Write("Inserisci un numero: ");
 int n = int.Parse(Console.ReadLine());
 
