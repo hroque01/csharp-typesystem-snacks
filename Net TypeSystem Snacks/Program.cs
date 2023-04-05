d2a81b254932c880655f1de64d837e22614a2743
Hadson Roque <nineonezero.light@gmail.com>
2023-04-05 14:28:40 +0200

    Create snack 3 ciclo for

--- Net TypeSystem Snacks/Program.cs
+++ Net TypeSystem Snacks/Program.cs
@@ -1,25 +1,21 @@
-﻿//Snack 1 Istruzioni condizionali
-Console.Write("Inserisci un numero: ");
-int num1 = int.Parse(Console.ReadLine());
+﻿////Snack 1 Istruzioni condizionali
+//Console.Write("Inserisci un numero: ");
+//int num1 = int.Parse(Console.ReadLine());
 
-Console.Write("Inserisci un numero: ");
-int num2 = int.Parse(Console.ReadLine());
+//Console.Write("Inserisci un numero: ");
+//int num2 = int.Parse(Console.ReadLine());
 
-if (num1<num2)
-{
-    Console.WriteLine($"il numero piu piccolo e': {num1}");
-}
-else
-{
-    Console.WriteLine($"il numero piu piccolo e': {num2}");
-}
-
-
+//if (num1 < num2)
+//{
+//    Console.WriteLine($"il numero piu piccolo e': {num1}");
+//}
+//else
+//{
+//    Console.WriteLine($"il numero piu piccolo e': {num2}");
+//}
 
 
-
-
-//Snack 2 Istruzioni condizionali
+////Snack 2 Istruzioni condizionali
 
 //Console.Write("Inserisci la prima parola: ");
 
@@ -33,22 +29,22 @@ else
 //    Console.WriteLine($"La parola piu' corta e': {parola1}");
 //}
 
-//else //altrimenti prendi il valore di num2
+//else
 //{
 //    Console.WriteLine($"La parola piu' corta e': {parola2}");
 //}
 
 
+//Snack 3 Ciclo for
+int somma = 0;
+for (int i = 0; i < 10; i++)
+{
+    Console.Write($"Inserisci il numero: ");
+    int numero = int.Parse(Console.ReadLine());
 
-
-//Ciclo FOR
-
-//for (int i = 0; i < 10; i++)
-//{   
-//    Console.Write($"Inserisci il numero: ");
-//    int numero = int.Parse( Console.ReadLine() );
-
-//}   
+    somma += numero;
+}
+Console.WriteLine("La somma di tutti i numeri inseriti è: " + somma);
 
 
 
