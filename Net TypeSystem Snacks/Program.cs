9f4e9a80df939f0f72b304322a8c0204e205b76e
Hadson Roque <nineonezero.light@gmail.com>
2023-04-05 14:25:25 +0200

    Create Snack 1 & 2 ISTRUZIONI CONDIZIONALI

--- Net TypeSystem Snacks/Program.cs
+++ Net TypeSystem Snacks/Program.cs
@@ -1,22 +1,54 @@
-﻿//Istruzioni condizionali
+﻿//Snack 1 Istruzioni condizionali
+Console.Write("Inserisci un numero: ");
+int num1 = int.Parse(Console.ReadLine());
 
-//Permette all'utente di scrivere un numero
-Console.Write("Inserisci il primo numero: ");
-    
-//Permette di prendere e assegnare la stringa num1 dalla console
-string num1 = Console.ReadLine();
+Console.Write("Inserisci un numero: ");
+int num2 = int.Parse(Console.ReadLine());
 
-Console.Write("Inserisci il secondo numero: ");
-    string num2 = Console.ReadLine();
-
-//SE la lunghezza di num1 e' minore della lunghezza di num2 prendi il valore di num1
-if (num1.Length<num2.Length)
+if (num1<num2)
 {
-    Console.WriteLine($"La parola piu' corta e': {num1}");
-} 
-
-else //altrimenti prendi il valore di num2
-{
-    Console.WriteLine($"La parola piu' corta e': {num2}");
+    Console.WriteLine($"il numero piu piccolo e': {num1}");
 }
+else
+{
+    Console.WriteLine($"il numero piu piccolo e': {num2}");
+}
+
+
+
+
+
+
+//Snack 2 Istruzioni condizionali
+
+//Console.Write("Inserisci la prima parola: ");
+
+//string parola1 = Console.ReadLine();
+
+//Console.Write("Inserisci la prima parola: ");
+//string parola2 = Console.ReadLine();
+
+//if (parola1.Length < parola2.Length)
+//{
+//    Console.WriteLine($"La parola piu' corta e': {parola1}");
+//}
+
+//else //altrimenti prendi il valore di num2
+//{
+//    Console.WriteLine($"La parola piu' corta e': {parola2}");
+//}
+
+
+
+
+//Ciclo FOR
+
+//for (int i = 0; i < 10; i++)
+//{   
+//    Console.Write($"Inserisci il numero: ");
+//    int numero = int.Parse( Console.ReadLine() );
+
+//}   
+
+
 
