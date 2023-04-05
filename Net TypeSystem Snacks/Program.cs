a3389a8325551568423b63b2600fa384e757c9bf
Hadson Roque <nineonezero.light@gmail.com>
2023-04-05 13:58:16 +0200

    Istruzioni condizionali

--- Net TypeSystem Snacks/Program.cs
+++ Net TypeSystem Snacks/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Istruzioni condizionali
+
+//Permette all'utente di scrivere un numero
+Console.Write("Inserisci il primo numero: ");
+    
+//Permette di prendere e assegnare la stringa num1 dalla console
+string num1 = Console.ReadLine();
+
+Console.Write("Inserisci il secondo numero: ");
+    string num2 = Console.ReadLine();
+
+//SE la lunghezza di num1 e' minore della lunghezza di num2 prendi il valore di num1
+if (num1.Length<num2.Length)
+{
+    Console.WriteLine($"La parola piu' corta e': {num1}");
+} 
+
+else //altrimenti prendi il valore di num2
+{
+    Console.WriteLine($"La parola piu' corta e': {num2}");
+}
+
